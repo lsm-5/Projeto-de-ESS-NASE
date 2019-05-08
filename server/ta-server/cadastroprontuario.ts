85f02c4fc09367d457c56f7848ffb537ff7a2b05
Jose Gabriel Silva Pereira <jgsp2@ug2c06.windows.cin.ufpe.br>
2019-05-08 14:56:36 -0300

    adicao do template de cadastro de prontuario

---
+++ server/ta-server/cadastroprontuario.ts
@@ -0,0 +1,41 @@
+//alterar pra prontuário
+import { Aluno } from '../../gui/ta-gui/src/app/aluno';
+
+export class CadastroDeAlunos {
+  alunos: Aluno[] = [];
+
+  criar(aluno: Aluno): Aluno {
+    var result = null;
+    if (this.cpfNaoCadastrado(aluno.cpf) && this.githubNaoCadastrado(aluno.github)) {
+      result = new Aluno();
+      result.copyFrom(aluno);
+      this.alunos.push(result);
+    }
+    return result;
+  }
+
+  cpfNaoCadastrado(cpf: string): boolean {
+     return !this.alunos.find(a => a.cpf == cpf);
+  }
+  githubNaoCadastrado(github: string): boolean {
+    return !this.alunos.find(a => a.github == github);
+  }
+  atualizar(aluno: Aluno): Aluno {
+    var result: Aluno = this.alunos.find(a => a.cpf == aluno.cpf);
+    if (result) result.copyFrom(aluno);
+    return result;
+  }
+
+  getAlunos(): Aluno[] {
+    return this.alunos;
+  }
+
+  remover(aluno:Aluno): Aluno[]{
+    //======================aqui
+    this.alunos = this.alunos.filter(a => a.cpf != aluno.cpf);
+    return this.alunos;
+    //==========================
+  }
+
+
+}
