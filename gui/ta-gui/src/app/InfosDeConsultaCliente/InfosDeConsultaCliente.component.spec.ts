e1b0790b949ce93e1e119a72aafd62bbb92bf365
juliano <jctv@cin.ufpe.br>
2019-06-16 10:33:35 -0300

    Deixando comentando o arquivo de teste
    ver dps como fazer os testes sem essas funcoes padroes

---
+++ gui/ta-gui/src/app/InfosDeConsultaCliente/InfosDeConsultaCliente.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { InfosDeConsultaClienteComponent } from './InfosDeConsultaCliente.component';
+
+// describe('InfosDeConsultaClienteComponent', () => {
+//   let component: InfosDeConsultaClienteComponent;
+//   let fixture: ComponentFixture<InfosDeConsultaClienteComponent>;
+
+//   beforeEach(async(() => {
+//     TestBed.configureTestingModule({
+//       declarations: [ InfosDeConsultaClienteComponent ]
+//     })
+//     .compileComponents();
+//   }));
+
+//   beforeEach(() => {
+//     fixture = TestBed.createComponent(InfosDeConsultaClienteComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
+
+//   it('should be created', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
