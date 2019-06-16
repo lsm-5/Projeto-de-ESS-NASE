8f71172ed511e6b7f6e76a6088ef4c6f2f39046a
juliano <jctv@cin.ufpe.br>
2019-06-16 09:15:48 -0300

    Atualizando corpo da classe AgendamentoEHistoricoCliente

--- gui/ta-gui/src/app/agendamento-ehistorico-cliente/agendamento-ehistorico-cliente.ts
+++ gui/ta-gui/src/app/agendamento-ehistorico-cliente/agendamento-ehistorico-cliente.ts
@@ -1,4 +1,38 @@
 export class AgendamentoEHistoricoCliente {
 
+    cpf: string; //do cliente do servico
+    nomeProfissional: string; //que vai atender
+    tipoDeConsulta: string; //mesmo que servico de atendimento
+    dataDaConsulta: string;
+    horarioConsulta: string;
+    datainicial: string; //para filtragem quando for mostrar o historico
+    datafinal: string;   //idem
+    historicoCompleto: boolean;
+    
+  
+    constructor() {
+      this.clean();
+    }
+  
+    clean(): void {
+      this.horarioConsulta = "";
+      this.tipoDeConsulta = "";
+      this.nomeProfissional = "";
+      this.cpf = "";
+      this.datainicial = "";
+      this.datafinal="";
+      this.historicoCompleto = false;
+      this.dataDaConsulta = "";
+    }
+    copyFrom(from: AgendamentoEHistoricoCliente): void {
+      this.horarioConsulta = from.horarioConsulta;
+      this.tipoDeConsulta = from.tipoDeConsulta;
+      this.nomeProfissional = from.nomeProfissional;
+      this.cpf = from.cpf;
+      this.datainicial = from.datainicial;
+      this.datafinal = from.datafinal;
+      this.historicoCompleto = from.historicoCompleto;
+      this.dataDaConsulta = from.dataDaConsulta;
+    }
   }
   
