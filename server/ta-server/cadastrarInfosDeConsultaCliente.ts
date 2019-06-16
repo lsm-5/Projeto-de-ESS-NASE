c02433f7bd4c782153ea5608af076f8f93bed822
juliano <jctv@cin.ufpe.br>
2019-06-16 19:33:08 -0300

    Atualizando algumas validacao para funcoes que serao usadas nas requisicoes

--- server/ta-server/cadastrarInfosDeConsultaCliente.ts
+++ server/ta-server/cadastrarInfosDeConsultaCliente.ts
@@ -6,7 +6,7 @@ export class CadastroDeInfosDeCosultas {
 
   criar(info: InfosDeConsultaCliente): InfosDeConsultaCliente {
     var result = null;
-    if (this.infoCosultaNaoCadastrada(info.cpf, info.dataDaConsulta, info.horarioConsulta)) {
+    if (this.infoCosultaNaoCadastrada(info.nomeProfissional, info.dataDaConsulta, info.horarioConsulta)) {
     //só pode criar um consulta se nao tiver cadastrada entao verifica-se cpf, data da consulta e hora. Nao se pode fazer duas consultas ao mesmo tempo.
         result = new InfosDeConsultaCliente();
         result.copyFrom(info);
@@ -16,12 +16,12 @@ export class CadastroDeInfosDeCosultas {
   }
 
   remover(info:InfosDeConsultaCliente): boolean {
-      this.infos = this.infos.filter(a => (a.cpf != info.cpf) && (a.dataDaConsulta != info.dataDaConsulta) && (a.horarioConsulta != info.horarioConsulta));
+      this.infos = this.infos.filter(a => (a.nomeProfissional != info.nomeProfissional) && (a.dataDaConsulta != info.dataDaConsulta) && (a.horarioConsulta != info.horarioConsulta));
       return true;
   }
 
-  infoCosultaNaoCadastrada(cpf: string, dataDaConsulta: string, horarioConsulta: string): boolean {
-    if(this.infos.find(a => (a.cpf == cpf) && (a.dataDaConsulta == dataDaConsulta) && (a.horarioConsulta == horarioConsulta)){
+  infoCosultaNaoCadastrada(nomeProfissional: string, dataDaConsulta: string, horarioConsulta: string): boolean {
+    if(this.infos.find(a => (a.nomeProfissional == nomeProfissional) && (a.dataDaConsulta == dataDaConsulta) && (a.horarioConsulta == horarioConsulta)){
       return false;
     }else{
       return true;
@@ -29,8 +29,8 @@ export class CadastroDeInfosDeCosultas {
   }
 
   atualizar(info: InfosDeConsultaCliente): InfosDeConsultaCliente {
-    if (!this.infoCosultaNaoCadastrada(info.cpf, info.dataDaConsulta, info.horarioConsulta)) {
-      var result: InfosDeConsultaCliente = this.infos.find(a => (a.cpf == info.cpf) && (a.dataDaConsulta == info.dataDaConsulta) && (a.horarioConsulta == info.horarioConsulta));
+    if (!this.infoCosultaNaoCadastrada(info.nomeProfissional, info.dataDaConsulta, info.horarioConsulta)) {
+      var result: InfosDeConsultaCliente = this.infos.find(a => (a.nomeProfissional == info.nomeProfissional) && (a.dataDaConsulta == info.dataDaConsulta) && (a.horarioConsulta == info.horarioConsulta));
       if (result) result.copyFrom(info);
       return result;
     }else{
