94f3004306fcdfc98bb604e75c931436034200c9
juliano <jctv@cin.ufpe.br>
2019-06-16 16:43:56 -0300

    Atualizando funcoes do servidor do component InfodeConsultas

--- server/ta-server/cadastrarInfosDeConsultaCliente.ts
+++ server/ta-server/cadastrarInfosDeConsultaCliente.ts
@@ -15,10 +15,10 @@ export class CadastroDeInfosDeCosultas {
     return result;
   }
 
-//   remover(info:InfosDeConsultaCliente): boolean {
-//       this.infos = this.infos.filter(a => (a.cpf != info.cpf) && (a.dataDaConsulta != info.dataDaConsulta) && (a.horarioConsulta != info.horarioConsulta));
-//       return true;
-//   }
+  remover(info:InfosDeConsultaCliente): boolean {
+      this.infos = this.infos.filter(a => (a.cpf != info.cpf) && (a.dataDaConsulta != info.dataDaConsulta) && (a.horarioConsulta != info.horarioConsulta));
+      return true;
+  }
 
   infoCosultaNaoCadastrada(cpf: string, dataDaConsulta: string, horarioConsulta: string): boolean {
     if(this.infos.find(a => (a.cpf == cpf) && (a.dataDaConsulta == dataDaConsulta) && (a.horarioConsulta == horarioConsulta)){
@@ -28,20 +28,20 @@ export class CadastroDeInfosDeCosultas {
     }
   }
 
-//   atualizar(info: InfosDeConsultaCliente): InfosDeConsultaCliente {
-//     if (!this.infoCosultaNaoCadastrada(info.cpf, info.dataDaConsulta, info.horarioConsulta)) {
-//       var result: InfosDeConsultaCliente = this.infos.find(a => (a.cpf == info.cpf) && (a.dataDaConsulta == info.dataDaConsulta) && (a.horarioConsulta == info.horarioConsulta));
-//       if (result) result.copyFrom(info);
-//       return result;
-//     }else{
-//       return null;
-//     }
-//   }
+  atualizar(info: InfosDeConsultaCliente): InfosDeConsultaCliente {
+    if (!this.infoCosultaNaoCadastrada(info.cpf, info.dataDaConsulta, info.horarioConsulta)) {
+      var result: InfosDeConsultaCliente = this.infos.find(a => (a.cpf == info.cpf) && (a.dataDaConsulta == info.dataDaConsulta) && (a.horarioConsulta == info.horarioConsulta));
+      if (result) result.copyFrom(info);
+      return result;
+    }else{
+      return null;
+    }
+  }
 
-//   busca(info: InfosDeConsultaCliente): InfosDeConsultaCliente[]{
-//     var result: InfosDeConsultaCliente [] = this.infos.filter(c => c.datainicial >= info.datainicial && c.datafinal<=info.datafinal);
-//     return result;
-//   }
+  busca(info: InfosDeConsultaCliente): InfosDeConsultaCliente[]{
+    var result: InfosDeConsultaCliente [] = this.infos.filter(c => c.datainicial >= info.datainicial && c.datafinal<=info.datafinal);
+    return result;
+  }
 
   getInfos(): InfosDeConsultaCliente[] {
     return this.infos;
